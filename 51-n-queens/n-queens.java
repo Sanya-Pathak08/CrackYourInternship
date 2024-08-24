753d02a119efcd3409aee4da5441e656ff985bb6
Sanya-Pathak08 <91409657+Sanya-Pathak08@users.noreply.github.com>
2024-08-24 20:48:44 +0530

    Time: 2 ms (89.55%) | Memory: 45.2 MB (6.37%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,49 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        backtrack(result, board, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<String>> result, char[][] board, int row) {
+        if (row == board.length) {
+            result.add(construct(board));
+            return;
+        }
+        for (int col = 0; col < board.length; col++) {
+            if (isValid(board, row, col)) {
+                board[row][col] = 'Q';
+                backtrack(result, board, row + 1);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isValid(char[][] board, int row, int col) {
+        int n = board.length;
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') return false;
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') return false;
+        }
+        return true;
+    }
+
+    private List<String> construct(char[][] board) {
+        List<String> res = new ArrayList<>();
+        for (char[] chars : board) {
+            res.add(new String(chars));
+        }
+        return res;
+    }
+}
